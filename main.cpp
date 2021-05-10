0dc9ce76090369d130409066d173e9f2c2817ff1
Kit <34525535+Kit-Law@users.noreply.github.com>
2021-05-11 00:52:07 +0100

    Removed redundancies

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include "bitmap/Bitmap.h"
-#include "bitmap/Header.h"
 
 extern "C"
 {
-	#include "os.h"
+	#include ".\lcd\lcd.h"
 }
 
 int main(void)
 {
-	os_init();
+	 /* 8MHz clock, no prescaling (DS, p. 48) */
+	CLKPR = (1 << CLKPCE);
+	CLKPR = 0;
 
-	sei();
+	DDRB |= _BV(PB7);  	 /* LED as output */
 
-	bitFortuna::Bitmap bm("bmp.BMP");
-	bm.display_debug();
-	bitFortuna::readPA(bm.getFile(), bm.getHeader());
+	init_lcd();
+
+	bitFortuna::Bitmap bm("pepe.BMP");
 
 	for (;;) {}
 
